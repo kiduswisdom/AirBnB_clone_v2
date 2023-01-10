62509a738c38fcc05e3a8975699a53cb28566f08
kiduswisdom <kidusjob@gmail.com>
2023-01-10 15:00:28 +0000

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Amenity model"""
+    """ class Amenity inherits from BaseModel and Base """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
+    # name = ""
