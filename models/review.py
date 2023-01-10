4311ba1ad4a19f9a4903c7109fbdc43542118535
kiduswisdom <kidusjob@gmail.com>
2023-01-10 14:09:45 +0000

    update

--- models/review.py
+++ models/review.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ The review class
-    """
-
+    """ Review classto store review information """
     __tablename__ = 'reviews'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # place_id = ""
+    # user_id = ""
+    # text = ""
